f2a83e14426c1655eaf4ae6d24dd5dafb433240f
Manojgowdapv <manojgowdapv9@gmail.com>
2026-04-21 18:18:27 +0530

    Create next.config.js file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['example.com'], // Replace with your image domains
+    },
+    env: {
+        CUSTOM_KEY: 'value', // Replace with your environment variables
+    },
+};
